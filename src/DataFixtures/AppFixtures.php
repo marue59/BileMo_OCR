9dd2e5e1f2567bc3857dba2bbaf06d0b6714ac8f
Marie <Marue59@hotmail.fr>
2022-01-18 19:21:47 +0100

    creation fixture for users and products

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Users;
+use App\Entity\Products;
 use App\Entity\Customers;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,19 +24,48 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+      
         $faker = Factory::create();
 
         for ($i = 0; $i < 5; $i++) {
             $customers = new Customers();
             $customers->setFullname($faker->name());
             $customers->setPassword($this->encoder->hashPassword($customers, 'password'));
-            $customers->setEmail("user$i@email.com");
+            $customers->setEmail("customers$i@email.com");
 
             $manager->persist($customers);
         }
-    
+
+        $users = $manager->getRepository(Users::class)->findAll();
+        $colors = ["blue", "red", "gold"];
+
+        for ($i = 0; $i < 15; $i++) {
+            $products = new Products();
+            $products->setBrand('marques'.$i);
+            $products->setModel('models'.$i);
+            $products->setColor($faker->randomElement($colors));
+            $products->setCapacity($faker->numberBetween(1, 20));
+            $products->setPrice($faker->numberBetween(50, 500));
+            $products->setDescription($faker->text());
+            $products->getUsers($faker->randomElement($users));
+
+            $manager->persist($products);
+        }
+
+        $products = $manager->getRepository(Products::class)->findAll();
+        $customers = $manager->getRepository(Customers::class)->findAll();
+
+
+        for ($i = 0; $i < 15; $i++) {
+            $users = new Users();
+            $users->setName($faker->name());
+            $users->getProducts($faker->randomElement($products));
+            $users->getCustomers($faker->randomElement($customers));
+            $users->setEmail("users$i@email.com");
+
+            $manager->persist($users);
+        }
+
         $manager->flush();
     }
 }
